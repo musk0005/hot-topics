0deedd7e481a11b92a48bd672bda14bf2373417f
musk0005 <122370077+musk0005@users.noreply.github.com>
2023-04-19 14:41:02 -0400

    Add files via upload

---
+++ load.js
@@ -0,0 +1,59 @@
+const container = document.querySelector('#maincontent');
+const links= document.querySelector("nav").querySelectorAll('a');
+let url = "partials/articals.txt";
+
+const content_load = function(clickedvalue){
+
+
+		switch (clickedvalue)
+	{
+
+		case 'index' :
+			url = "partials/articals.txt"
+			break;
+
+		case 'portfolio':
+			
+			url = "partials/portfolio.txt";
+			break;
+
+		default:
+
+			console.log('not clicked');
+	}
+
+
+	
+
+
+	
+fetch(url)
+	.then((response) =>  {
+		return response.text()
+	})
+	.then(data => {
+		container.innerHTML=data
+	})
+	.catch(error => 
+		{
+			console.log('Error - no file fetched')
+		});
+
+}
+
+content_load()
+
+const selectContent=function(event){
+
+	event.preventDefault();
+	let anchor_value = this.getAttribute('href');
+	content_load(anchor_value);
+	
+}
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', selectContent, false);
+    
+
+}
+
